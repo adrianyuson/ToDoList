ade05f711a55e9d596c3db6464e77e9a2a8744e1
Adrian Yuson <adrianyuson88@yahoo.com>
2017-10-18 07:21:11 -0700

    Progress

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,8 +1,18 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(event) {
-    $(this).parent().remove();
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(200, function(event) {
+		$(this).remove();
+	});
     event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event) {
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><button>X</button></span>" + todoText + "</li>");
+	}	
 });
